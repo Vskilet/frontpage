be5bd08e2a0a617bffe7988904d9aa1f0f534cfb
Victor SENE <victor@sene.ovh>
2021-05-13 14:44:05 +0200

    start project

---
+++ engagements/js/example.js
@@ -0,0 +1,24 @@
+Plotly.d3.csv('data/coffee-flavors.csv', function(err, rows){
+    function unpack(rows, key) {
+    return rows.map(function(row) {return row[key]})
+  }
+
+  var data = [{
+    type: "sunburst",
+    maxdepth: 3,
+    ids: unpack(rows, 'ids'),
+    labels: unpack(rows, 'labels'),
+    parents: unpack(rows, 'parents'),
+    hovertext: unpack(rows, 'text'),
+    showlegend: 'false',
+    insidetextorientation: 'radial'
+  }]
+
+  var layout = {
+    margin: {l: 0, r: 0, b: 0, t:0},
+    width: 800,
+    height: 800
+  }
+
+  Plotly.newPlot('myDiv', data, layout)
+})
